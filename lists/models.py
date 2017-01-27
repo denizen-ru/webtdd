de8dea7c31c60781a697182941b5f11693e6b074
Denis Knyazev <denis.m.knyazev@gmail.com>
2017-01-27 22:31:50 +0300

    Add model List, change migrations

--- lists/models.py
+++ lists/models.py
@@ -1,8 +1,19 @@
 from django.db import models
 
 
+class List(models.Model):
+
+    class Meta:
+        verbose_name = "List"
+        verbose_name_plural = "Lists"
+
+    def __str__(self):
+        pass
+
+
 class Item(models.Model):
     text = models.TextField(default='')
+    list = models.ForeignKey(List, default=None)
 
     class Meta:
         verbose_name = "Item"
