e120518ab71ed9bb9ed7f4e663237f77fde56e66
Denis Knyazev <denis.m.knyazev@gmail.com>
2017-02-09 04:47:38 +0300

    Fix all FTs

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,11 @@
 from django.conf.urls import url
+from django.contrib.auth.views import logout
+
 from accounts import views as accounts_views
 
 urlpatterns = [
     url(r'^send_login_email$', accounts_views.send_login_email,
         name='send_login_email'),
-    url(r'^login$', accounts_views.login, name='login')
+    url(r'^login$', accounts_views.login, name='login'),
+    url(r'^logout$', logout, {'next_page': '/'}, name='logout'),
 ]
