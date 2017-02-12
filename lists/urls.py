0dbd6ef7b49837a417fcf98aaaead5527126ad04
Denis Knyazev <denis.m.knyazev@gmail.com>
2017-02-12 17:45:27 +0300

    new_list view tries to assign owner but cant

--- lists/urls.py
+++ lists/urls.py
@@ -4,5 +4,5 @@ from lists import views as lists_views
 urlpatterns = [
     url(r'^(\d+)/$', lists_views.view_list, name='view_list'),
     url(r'^new$', lists_views.new_list, name='new_list'),
-    url(r'^users/(.+)$', lists_views.my_lists, name='my_lists'),
+    url(r'^users/(.+)/$', lists_views.my_lists, name='my_lists'),
 ]
